bfe6eee2c6776f1ae10cecbc03a6ab30245ebd23
jhs512 <jangka512@gmail.com>
2021-07-17 17:46:48 +0900

    세팅

---
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -0,0 +1,14 @@
+package com.sbs.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요.";
+	}
+}

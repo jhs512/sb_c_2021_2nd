fdb656f1ce426a21ae4a05fdabc689d041630296
jhs512 <jangka512@gmail.com>
2021-07-18 17:15:06 +0900

    count 조절 액션 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -7,40 +7,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UsrHomeController {
 	private int count;
-	
+
 	public UsrHomeController() {
 		count = -1;
 	}
-	
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕하세요.";
-	}
-	
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다.";
-	}
-	
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또 만나요.";
-	}
 
-	@RequestMapping("/usr/home/main4")
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
-	public int showMain4() {
-		count++;
+	public int getCount() {
 		return count;
 	}
-	
-	@RequestMapping("/usr/home/main5")
+
+	@RequestMapping("/usr/home/doSetCount")
 	@ResponseBody
-	public String showMain5() {
-		count = 0;
-		return "count의 값이 0으로 초기화 되었습니다.";
+	public String doSetCount(int count) {
+		this.count = count;
+		return "count의 값이 " + this.count + "으로 초기화 되었습니다.";
+
 	}
 }

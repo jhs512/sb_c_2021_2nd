0dd55b009d0da6ede5272271ec4ea0c1e7508523
jhs512 <jangka512@gmail.com>
2021-07-18 16:34:30 +0900

    /usr/home/main2, /usr/home/main3 추가

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -11,4 +11,16 @@ public class UsrHomeController {
 	public String showMain() {
 		return "안녕하세요.";
 	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다.";
+	}
+	
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또 만나요.";
+	}
 }

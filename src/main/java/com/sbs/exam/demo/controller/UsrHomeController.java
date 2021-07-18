4d66d888ff4398820fe25f116acf10f4a552b0bb
jhs512 <jangka512@gmail.com>
2021-07-18 17:07:36 +0900

    count변수의 값 초기화 액션 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -36,4 +36,11 @@ public class UsrHomeController {
 		count++;
 		return count;
 	}
+	
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count의 값이 0으로 초기화 되었습니다.";
+	}
 }

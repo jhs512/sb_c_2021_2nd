576389bd757933ff25e263c6c77b4b637258ac61
jhs512 <jangka512@gmail.com>
2021-07-24 16:16:38 +0900

    게시물 수정기능

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -59,6 +59,13 @@ public class UsrArticleController {
 		
 		articles.remove(article);
 	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+	}
 	// 서비스 메서드 끝
 
 	// 액션 메서드 시작
@@ -88,6 +95,20 @@ public class UsrArticleController {
 		deleteArticle(id);
 		
 		return id + "번 게시물을 삭제하였습니다.";
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if ( article == null ) {
+			return id + "번 게시물이 존재하지 않습니다.";			
+		}
+		
+		modifyArticle(id, title, body);
+		
+		return id + "번 게시물을 수정하였습니다.";
 	}
 	// 액션 메서드 끝
 }

f358c3e08a285df77a3d9235513c8e910b6332e0
jhs512 <jangka512@gmail.com>
2021-07-24 15:54:06 +0900

    getArticle, deleteArticle 서비스 메서드로 게시물 삭제기능 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -11,9 +11,12 @@ import com.sbs.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
+	// 인스턴스 변수 시작
 	private int articlesLastId;
 	private List<Article> articles;
+	// 인스턴스 변수 끝
 
+	// 생성자
 	public UsrArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
@@ -21,6 +24,7 @@ public class UsrArticleController {
 		makeTestData();
 	}
 
+	// 서비스 메서드 시작
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
 			String title = "제목 " + i;
@@ -39,7 +43,25 @@ public class UsrArticleController {
 		
 		return article;
 	}
+	
+	private Article getArticle(int id) {
+		for ( Article article : articles ) {
+			if ( article.getId() == id ) {
+				return article;
+			}
+		}
+		
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+	}
+	// 서비스 메서드 끝
 
+	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -57,7 +79,15 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
-
+		Article article = getArticle(id);
+		
+		if ( article == null ) {
+			return id + "번 게시물이 존재하지 않습니다.";			
+		}
+		
+		deleteArticle(id);
+		
 		return id + "번 게시물을 삭제하였습니다.";
 	}
+	// 액션 메서드 끝
 }

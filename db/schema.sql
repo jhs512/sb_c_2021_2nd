a8cbba2abfc2af3a372354733a71b86a36520d63
jhs512 <jangka512@gmail.com>
2021-07-25 15:45:10 +0900

    db/schema.sql 파일추가

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+# DB 생성
+DROP DATABASE IF EXISTS sb_c_2021_2nd_t;
+CREATE DATABASE sb_c_2021_2nd_t;
+USE sb_c_2021_2nd_t;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+# 게시물, 테스트 데이터 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 1',
+`body` = '내용 1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 2',
+`body` = '내용 2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 3',
+`body` = '내용 3';

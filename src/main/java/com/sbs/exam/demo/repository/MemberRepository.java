cc3b08d39183cbf2ecce8e3eed1251758a2a83b3
jhs512 <jangka512@gmail.com>
2021-07-31 16:29:28 +0900

    MemberRepository 추가 후, 회원가입 처리

---
+++ src/main/java/com/sbs/exam/demo/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.sbs.exam.demo.repository;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberRepository {
+	@Insert("""
+			INSERT INTO `member`
+			SET regDate = NOW(),
+			updateDate = NOW(),
+			loginId = #{loginId},
+			loginPw = #{loginPw},
+			`name` = #{name},
+			nickname = #{nickname},
+			cellphoneNo = #{cellphoneNo},
+			email = #{email}
+						""")
+	void join(@Param("loginId") String loginId, @Param("loginPw") String loginPw, @Param("name") String name,
+			@Param("nickname") String nickname, @Param("cellphoneNo") String cellphoneNo, @Param("email") String email);
+}
--- src/main/java/com/sbs/exam/demo/service/MemberService.java
+++ src/main/java/com/sbs/exam/demo/service/MemberService.java
@@ -2,11 +2,17 @@ package com.sbs.exam.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sbs.exam.demo.repository.MemberRepository;
+
 @Service
 public class MemberService {
-	//private MemberRepository memberRepository;
+	private MemberRepository memberRepository;
+
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		//memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 	}
 }

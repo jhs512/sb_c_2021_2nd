88a5ebd320524eb90d85e4cfca6282b9bef9de48
jhs512 <jangka512@gmail.com>
2021-07-31 17:49:04 +0900

    회원가입시, 이름+이메일 중복체크

--- src/main/java/com/sbs/exam/demo/repository/MemberRepository.java
+++ src/main/java/com/sbs/exam/demo/repository/MemberRepository.java
@@ -39,4 +39,12 @@ public interface MemberRepository {
 			WHERE M.loginId = #{loginId}
 			""")
 	Member getMemberByLoginId(@Param("loginId") String loginId);
+
+	@Select("""
+			SELECT *
+			FROM `member` AS M
+			WHERE M.name = #{name}
+			AND M.email = #{email}
+			""")
+	Member getMemberByNameAndEmail(@Param("name") String name, @Param("email") String email);
 }

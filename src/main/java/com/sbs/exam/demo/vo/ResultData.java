59d8c73dfa28cbd5b492be5c7f725acf9741cbb0
jhs512 <jangka512@gmail.com>
2021-08-01 16:18:39 +0900

    범용 보고서 클래스, ResultData 구현

---
+++ src/main/java/com/sbs/exam/demo/vo/ResultData.java
@@ -0,0 +1,33 @@
+package com.sbs.exam.demo.vo;
+
+import lombok.Getter;
+
+public class ResultData {
+	@Getter
+	private String resultCode;
+	@Getter
+	private String msg;
+	@Getter
+	private Object data1;
+	
+	private ResultData() {
+		
+	}
+	
+	public static ResultData from(String resultCode, String msg, Object data1) {
+		ResultData rd = new ResultData();
+		rd.resultCode = resultCode;
+		rd.msg = msg;
+		rd.data1 = data1;
+		
+		return rd;
+	}
+	
+	public boolean isSuccess() {
+		return resultCode.startsWith("S-");
+	}
+	
+	public boolean isFail() {
+		return isSuccess() == false;
+	}
+}

fe64e97a5211343e21d6a906b9bc4e296e05fe44
jhs512 <jangka512@gmail.com>
2021-08-01 16:35:22 +0900

    /usr/article/getArticles 에 ResultData 적용

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -39,8 +39,10 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public List<Article> getArticles() {
-		return articleService.getArticles();
+	public ResultData getArticles() {
+		List<Article> articles = articleService.getArticles();
+		
+		return ResultData.from("S-1", "게시물 리스트 입니다.", articles);
 	}
 
 	@RequestMapping("/usr/article/getArticle")

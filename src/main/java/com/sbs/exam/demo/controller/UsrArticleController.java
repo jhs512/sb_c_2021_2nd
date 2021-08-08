b9b3a5047ca02e92ed0eab018a1791b048bfa781
jhs512 <jangka512@gmail.com>
2021-08-08 15:34:41 +0900

    게시물 상세페이지 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -60,6 +60,15 @@ public class UsrArticleController {
 
 		return "usr/article/list";
 	}
+	
+	@RequestMapping("/usr/article/detail")
+	public String showDetail(Model model, int id) {
+		Article article = articleService.getArticle(id);
+		
+		model.addAttribute("article", article);
+
+		return "usr/article/detail";
+	}
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody

b07a2bb79c86b5666c7cd5dccf93cbffd1a223b3
jhs512 <jangka512@gmail.com>
2021-08-14 17:47:57 +0900

    게시물 수정 폼

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -100,7 +100,7 @@ public class UsrArticleController {
 	}
 
 	@RequestMapping("/usr/article/modify")
-	public String doModify(HttpServletRequest req, int id) {
+	public String showModify(HttpServletRequest req, Model model, int id) {
 		Rq rq = (Rq) req.getAttribute("rq");
 
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
@@ -114,6 +114,8 @@ public class UsrArticleController {
 		if (actorCanModifyRd.isFail()) {
 			return rq.historyBackJsOnView(actorCanModifyRd.getMsg());
 		}
+		
+		model.addAttribute("article", article);
 
 		return "usr/article/modify";
 	}

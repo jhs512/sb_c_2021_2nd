65f969f564baafc0eb880c92792da3cb42b7e77a
jhs512 <jangka512@gmail.com>
2021-08-21 16:32:22 +0900

    BeforeActionInterceptor 에서 Rq 객체를 좀 더 확실하게 생성되도록 유도

--- src/main/java/com/sbs/exam/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/sbs/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -3,19 +3,23 @@ package com.sbs.exam.demo.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.sbs.exam.demo.service.MemberService;
 import com.sbs.exam.demo.vo.Rq;
 
 @Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
+	private Rq rq;
+	
+	public BeforeActionInterceptor(Rq rq) {
+		this.rq = rq;
+	}
+	
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		// 이제는 Rq 객체가 자동으로 만들어지기 때문에 필요 없음
-
+		rq.initOnBeforeActionInterceptor();
+		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 }
--- src/main/java/com/sbs/exam/demo/vo/Rq.java
+++ src/main/java/com/sbs/exam/demo/vo/Rq.java
@@ -48,7 +48,7 @@ public class Rq {
 		this.isLogined = isLogined;
 		this.loginedMemberId = loginedMemberId;
 		this.loginedMember = loginedMember;
-		
+
 		this.req.setAttribute("rq", this);
 	}
 
@@ -89,5 +89,12 @@ public class Rq {
 
 	public String jsReplace(String msg, String uri) {
 		return Ut.jsReplace(msg, uri);
+	}
+
+	// 이 메서드는 Rq 객체가 자연스럽게 생성되도록 유도하는 역할을 한다.
+	// 지우면 안되고,
+	// 편의를 위해 BeforeActionInterceptor 에서 꼭 호출해줘야 한다.
+	public void initOnBeforeActionInterceptor() {
+
 	}
 }

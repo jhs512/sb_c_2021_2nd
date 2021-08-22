53e1f5c666505661d753706f2e9c07c1da31f1e7
jhs512 <jangka512@gmail.com>
2021-08-22 17:06:51 +0900

    검색박스 구현

--- src/main/resources/static/resource/common.js
+++ src/main/resources/static/resource/common.js
@@ -1,21 +1,9 @@
-@font-face {
-  font-family: 'LotteMartDream';
-  font-style: normal;
-  font-weight: 400;
-  src: url('https://cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamMedium.woff2') format('woff2'), url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamMedium.woff') format('woff');
-}
-@font-face {
-  font-family: 'LotteMartDream';
-  font-style: normal;
-  font-weight: 700;
-  src: url('https://cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamBold.woff2') format('woff2'), url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamBold.woff') format('woff');
-}
-@font-face {
-  font-family: 'LotteMartDream';
-  font-style: normal;
-  font-weight: 300;
-  src: url('https://cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamLight.woff2') format('woff2'), url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamLight.woff') format('woff');
-}
-html > body {
- font-family: 'LotteMartDream', sans-serif;
-}
+$('select[data-value]').each(function(index, el) {
+  const $el = $(el);
+  
+  const defaultValue = $el.attr('data-value').trim();
+  
+  if ( defaultValue.length > 0 ) {
+	$el.val(defaultValue);	
+  }
+});
--- src/main/webapp/WEB-INF/jsp/usr/article/list.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/article/list.jsp
@@ -6,7 +6,26 @@
 
 <section class="mt-5">
   <div class="container mx-auto px-3">
-    <div>게시물 개수 : ${articlesCount}개</div>
+    <div class="flex">
+      <div>
+        게시물 개수 : <span class="badge badge-primary">${articlesCount}</span>개
+      </div>
+      <div class="flex-grow"></div>
+      <form class="flex">
+        <input type="hidden" name="boardId" value="${param.boardId}" />
+      
+        <select data-value="${param.searchKeywordTypeCode}" name="searchKeywordTypeCode" class="select select-bordered">
+          <option disabled="disabled">검색타입</option>
+          <option value="title">제목</option>
+          <option value="body">내용</option>
+          <option value="title,body">제목,내용</option>
+        </select>
+        
+        <input name="searchKeyword" type="text" class="ml-2 w-72 input input-bordered" placeholder="검색어" maxlength="20" value="${param.searchKeyword}" />
+        
+        <button type="submit" class="ml-2 btn btn-primary">검색</button>
+      </form>
+    </div>
     <div class="mt-3">
       <table class="table table-fixed w-full">
         <colgroup>

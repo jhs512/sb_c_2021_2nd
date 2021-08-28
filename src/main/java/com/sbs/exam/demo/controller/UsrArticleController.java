2b4ad41af926dbd4f8d49aabe3218ec034e6bae9
jhs512 <jangka512@gmail.com>
2021-08-28 15:42:33 +0900

    조회수 증가 성공에 대한 답장에, data2(글 번호) 정보 추가

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -75,7 +75,11 @@ public class UsrArticleController {
 			return increaseHitCountRd;
 		}
 
-		return ResultData.newData(increaseHitCountRd, "hitCount", articleService.getArticleHitCount(id));
+		ResultData<Integer> rd = ResultData.newData(increaseHitCountRd, "hitCount", articleService.getArticleHitCount(id));
+		
+		rd.setData2("id", id);
+		
+		return rd;
 	}
 
 	@RequestMapping("/usr/article/getArticle")

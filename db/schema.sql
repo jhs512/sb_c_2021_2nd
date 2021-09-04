8a96284a5527c054759823b80c2acbdac9caa35d
jhs512 <jangka512@gmail.com>
2021-09-04 16:58:46 +0900

    댓글 테이블에 인덱스 걸기

--- db/schema.sql
+++ db/schema.sql
@@ -272,4 +272,12 @@ relTypeCode = 'article',
 relId = 2,
 `body` = '댓글 4';
 
-SELECT * FROM reply
+# 댓글에 좋아요 수, 싫어요 수 칼럼 추가
+ALTER TABLE reply
+ADD COLUMN goodReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
+
+ALTER TABLE reply
+ADD COLUMN badReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
+
+# 댓글 테이블에 인덱스 걸기
+ALTER TABLE `reply` ADD INDEX (`relTypeCode` , `relId`);
